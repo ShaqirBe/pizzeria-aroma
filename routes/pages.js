9bf1f433aa693e4c3f736d2fc711adb9778b6625
Shaqir Beqiri <shaqir.82@hotmail.com>
2026-02-20 19:56:00 +0100

    Add SEO metadata for pages

--- routes/pages.js
+++ routes/pages.js
@@ -7,9 +7,15 @@ const { isOpenNow, getNextOpening } = require("../utils/isOpenNow");
 // IMPORT I SAKTË
 const menuData = require("../data/menu.json");
 const allergensData = require("../data/allergens.json");
+const SITE_URL = "https://www.pizzeria-aroma-peckelsheim.de";
 
 router.use((req, res, next) => {
   res.locals.currentPath = req.path;
+  res.locals.pageTitle = "Pizzeria Aroma Peckelsheim - Pizza, Pasta & Salat";
+  res.locals.pageDescription =
+    "Pizzeria Aroma in Peckelsheim. Frische Pizza, Pasta und Salat. Jetzt anrufen oder Route anzeigen. Familienbetrieb.";
+  res.locals.canonicalUrl = `${SITE_URL}${req.path}`;
+  res.locals.pageImage = `${SITE_URL}/images2/fasade.jpeg`;
   next();
 });
 
@@ -29,25 +35,39 @@ router.get("/", (req, res) => {
   res.render("index", {
     imagesJSON: JSON.stringify(imageFiles),
     isOpen: open,
-    nextOpening
+    nextOpening,
+    pageTitle: "Pizzeria Aroma Peckelsheim - Pizza, Pasta & Salat",
+    pageDescription:
+      "Pizzeria Aroma in Peckelsheim. Frische Pizza, Pasta und Salat. Jetzt anrufen oder Route anzeigen. Familienbetrieb."
   });
 });
 
 
 router.get("/menu", (req, res) => {
-  res.render("menu", { menu: menuData,
-    allergens: allergensData
-   });
+  res.render("menu", {
+    menu: menuData,
+    allergens: allergensData,
+    pageTitle: "Speisekarte | Pizzeria Aroma Peckelsheim",
+    pageDescription:
+      "Unsere aktuelle Speisekarte mit Pizza, Pasta, Salat, Burgern und Getränken in Peckelsheim."
+  });
 });
 
 
 
 router.get("/kontakt", (req, res) => {
-  res.render("kontakt");
+  res.render("kontakt", {
+    pageTitle: "Kontakt | Pizzeria Aroma Peckelsheim",
+    pageDescription:
+      "Kontakt zur Pizzeria Aroma in Peckelsheim: Adresse, Telefon und E-Mail auf einen Blick."
+  });
 });
 
 router.get("/impressum", (req, res) => {
-  res.render("impressum");
+  res.render("impressum", {
+    pageTitle: "Impressum | Pizzeria Aroma Peckelsheim",
+    pageDescription: "Impressum und rechtliche Angaben der Pizzeria Aroma Peckelsheim."
+  });
 });
 
 module.exports = router;
